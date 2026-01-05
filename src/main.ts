796500fbb284a7307c04b1e5c3493921ff8b296a
akiGIt <naofk221512@gmail.com>
2026-01-05 13:52:17 +0900

    ポート設定を変更

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //CORSの緩和
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port ?? 3000);
 }
 bootstrap();
