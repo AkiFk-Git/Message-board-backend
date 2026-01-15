d1e8f664e21fdc99ee582fe1f97874b9b1eb4e86
akiGIt <naofk221512@gmail.com>
2026-01-15 11:32:43 +0900

    デコレータ追加

--- src/dto/signUp.dto.ts
+++ src/dto/signUp.dto.ts
@@ -1,8 +1,10 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 //サインアップ処理時のDTO
 export class SignUpDto {
+  @IsString()
   name: string;
+  @IsString()
   password: string;
   @IsEmail()
   mail: string;
